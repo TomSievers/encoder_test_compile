094423f537a364c83eff56b829c738ae7d5a4ec7
TomSievers <tom.sievers13@gmail.com>
2020-07-13 13:50:04 +0200

    tested encoder compile on windows, does not work undefined reference

---
+++ build/CMakeFiles/encoder_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encoder_test.dir/src/Encoder.cpp.obj"
+  "CMakeFiles/encoder_test.dir/src/main.cpp.obj"
+  "encoder_test.exe"
+  "encoder_test.exe.manifest"
+  "encoder_test.pdb"
+  "libencoder_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encoder_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
