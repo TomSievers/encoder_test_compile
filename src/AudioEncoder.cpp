f4c77929478de930f8e7b6a42e61a2cbc6e6c709
TomSievers <tom.sievers13@gmail.com>
2020-08-05 13:09:16 +0200

    missed saving

--- src/AudioEncoder.cpp
+++ src/AudioEncoder.cpp
@@ -3,8 +3,31 @@
 AudioEncoder::AudioEncoder(/* args */)
     : Codec(AV_CODEC_ID_ALAC, avcodec_find_encoder)
 {
+        c->sample_fmt  = (*codec)->sample_fmts ?
+        (*codec)->sample_fmts[0] : AV_SAMPLE_FMT_FLTP;
+        c->bit_rate    = 64000;
+        c->sample_rate = 44100;
+        if ((*codec)->supported_samplerates) {
+            c->sample_rate = (*codec)->supported_samplerates[0];
+            for (i = 0; (*codec)->supported_samplerates[i]; i++) {
+                if ((*codec)->supported_samplerates[i] == 44100)
+                    c->sample_rate = 44100;
+            }
+        }
+        c->channels        = av_get_channel_layout_nb_channels(c->channel_layout);
+        c->channel_layout = AV_CH_LAYOUT_STEREO;
+        if ((*codec)->channel_layouts) {
+            c->channel_layout = (*codec)->channel_layouts[0];
+            for (i = 0; (*codec)->channel_layouts[i]; i++) {
+                if ((*codec)->channel_layouts[i] == AV_CH_LAYOUT_STEREO)
+                    c->channel_layout = AV_CH_LAYOUT_STEREO;
+            }
+        }
+        c->channels        = av_get_channel_layout_nb_channels(c->channel_layout);
+        ost->st->time_base = (AVRational){ 1, c->sample_rate };
 }
 
 AudioEncoder::~AudioEncoder()
 {
+
 }
